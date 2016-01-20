fb2c75afe79c1fad0dbc608a530a6d1a39b4da9f
pawelchojnacki <chojnacki.pawel@linux.com>
2016-01-20 15:06:04 +0100

    allow multiple configs with webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
-
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: PATHS.app,
   output: {
     path: PATHS.build,
@@ -19,3 +20,12 @@ module.exports = {
     })
   ]
 };
+
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if (TARGET = 'build') {
+  module.exports = merge(common, {});
+}
