f2fb3e229f9010cc0aec12c7d784503a23a721b8
pawelchojnacki <chojnacki.pawel@linux.com>
2016-01-24 12:14:25 +0100

    connecting flux architecture to the existing app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,28 +1,26 @@
-import uuid from 'node-uuid';
 import React from 'react';
 import Note from './Note.jsx';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Read Nikolas blog'
-        }
-      ]
-    }
+    this.state = NoteStore.getState();
   }
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged = (state) => {
+    // Without a property initializer `this` wouldn't
+    // point at the right context because it defaults to
+    // `undefined` in strict mode.
+    this.setState(state);
+  };
   render() {
     const notes = this.state.notes;
     return (
@@ -35,26 +33,13 @@ export default class App extends React.Component {
       </div>
     );
   };
-  addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    });
-  };
-  editNote = (id, task) => {
-    const notes = this.state.notes.map(note => {
-      if (note.id === id && task) {
-        note.task = task;
-      }
-      return note;
-    });
-    this.setState({notes});
-  };
-  deleteNote = (id) => {
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  };
+  addNote() {
+    NoteActions.create({task: 'New task'});
+  }
+  editNote(id, task) {
+    NoteActions.update({id, task});
+  }
+  deleteNote(id) {
+    NoteActions.delete(id);
+  }
 }
