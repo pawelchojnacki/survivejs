0727cf11cad88eaffd55bd0d98086f206552b443
pawelchojnacki <chojnacki.pawel@linux.com>
2016-01-25 19:12:12 +0100

    dnd logic completely implemented

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,6 +1,7 @@
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
+import update from 'react-addons-update';
 
 class LaneStore {
   constructor() {
@@ -52,7 +53,30 @@ class LaneStore {
     this.setState({lanes});
   }
   move({sourceId, targetId}) {
-    console.log(`source: ${sourceId}, target: ${targetId}`);
+    const lanes = this.lanes;
+    const sourceLane = lanes.filter(lane => {
+      return lane.notes.indexOf(sourceId) >= 0;
+    })[0];
+    const targetLane = lanes.filter(lane => {
+      return lane.notes.indexOf(targetId) >= 0;
+    })[0];
+    const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+    const targetNoteIndex = targetLane.notes.indexOf(targetId);
+    if (sourceLane === targetLane) {
+      // move at once to avoid complications
+      sourceLane.notes = update(sourceLane.notes, {
+        $splice: [
+          [sourceNoteIndex, 1],
+          [targetNoteIndex, 0, sourceId]
+        ]
+      });
+    } else {
+      // get rid of the source
+      sourceLane.notes.splice(sourceNoteIndex, 1);
+      // and move to target
+      targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+    }
+    this.setState({lanes});
   }
 }
 
