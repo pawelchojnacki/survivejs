a13896310c6985f4ff529f488ae4127cce466221
pawelchojnacki <chojnacki.pawel@linux.com>
2016-01-27 21:05:24 +0100

    editable test: onValueClick

--- tests/editable_test.jsx
+++ tests/editable_test.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {
   renderIntoDocument,
-  findRenderedDOMComponentWithClass
+  findRenderedDOMComponentWithClass,
+  findRenderedDOMComponentWithTag,
+  Simulate
 } from 'react-addons-test-utils';
 import assert from 'assert';
 import Editable from '../app/components/Editable.jsx';
@@ -15,4 +17,16 @@ describe('Editable', () => {
     const valueComponent = findRenderedDOMComponentWithClass(component, 'value');
     assert.equal(valueComponent.textContent, value);
   });
+
+  it('triggers onValueClick', () => {
+    let triggered = false;
+    const value = 'value';
+    const onValueClick = () => triggered = true;
+    const component = renderIntoDocument(
+      <Editable value={value} onValueClick={onValueClick} />
+    );
+    const valueComponent = findRenderedDOMComponentWithClass(component, 'value');
+    Simulate.click(valueComponent);
+    assert.equal(triggered, true);
+  })
 });
